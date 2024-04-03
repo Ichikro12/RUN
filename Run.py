141e6e7f572466725bcc05e132df11a385885616
Ichikro12 <164194889+Ichikro12@users.noreply.github.com>
2024-04-03 19:31:08 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,20 +1,13 @@
-import streamlit as st
 import subprocess
+import streamlit as st
 
-def start_streamlit():
-    process = subprocess.Popen(['streamlit', 'run', 'iitbombay-main/pythonproject1/main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+st.write('Click to start innovarium module')
+if st.button('Start'):
+    # Run Streamlit app
+    process = subprocess.Popen(['streamlit', 'run', 'main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Wait for Streamlit app to start
+    process.stdout.readline()
+
+    # Wait for the Streamlit app to finish
     process.wait()
-
-def main():
-    st.title("Streamlit App Starter")
-
-    # Function to start the Streamlit app
-    def start_app():
-        start_streamlit()
-
-    # Button to start the app
-    if st.button("Start App"):
-        start_app()
-
-if __name__ == "__main__":
-    main()
